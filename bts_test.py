bdb414608f46a1f6d2b3924d06d1da76d46ce1f4
scratcher221 <mitterlehner.david@gmail.com>
2019-07-29 01:43:25 +0200

    Progressed the implementation of bts algorithm.

--- bts_test.py
+++ bts_test.py
@@ -1,9 +1,20 @@
+import math
+
 class Vote:
     def __init__(self, answer, prediction):
         self.answer = answer
         self.prediction = prediction
 
-def bts(votes):
+def bts(num_answer_options, votes):
+    # Check for errors (Total prediction percentage less than or greater than 100):
+    for vote in votes:
+        total_prediction_percentage = 0
+        for answer in vote.prediction:
+            total_prediction_percentage += vote.prediction[answer]
+        if total_prediction_percentage != 100:
+            print("Error, total prediction must add up to 100% for each respondent.")
+            return False
+    # Print all the votes and predictions:
     respondent_index = 0
     for vote in votes:
         print(F"Answer of respondent {respondent_index + 1}: {vote.answer}")
@@ -12,6 +23,42 @@ def bts(votes):
             print(F"Answer: {answer}, Percentage: {vote.prediction[answer]}")
         print("")
         respondent_index += 1
+    # Calculate the total votes and endorsement frequencies for answer k:
+    total_votes = []
+    endorsement_frequencies = []
+    for i in range(num_answer_options):
+        total_votes.append(0)
+        endorsement_frequencies.append(0)
+    for vote in votes:
+        total_votes[vote.answer - 1] += 1
+    # Print the total votes:
+    for i in range(len(total_votes)):
+        print(F"Answer {i + 1} has {total_votes[i]} vote(s)")
+        endorsement_frequencies[i] = total_votes[i] / len(votes)
+        print(F"Endorsement frequencies vor answer {i + 1}: {round(endorsement_frequencies[i] * 100, 2)}%")
+    # Calculate the geometric (and arithmetic for testing) average of predicted frequencies:
+    geometric_avg_predicted_frequencies = []
+    arithmetic_avg_predicted_frequencies = []
+    predicted_frequencies = []
+    for i in range(num_answer_options):
+        geometric_avg_predicted_frequencies.append(0)
+        arithmetic_avg_predicted_frequencies.append(0)
+        predicted_frequencies.append(0)
+    for vote in votes:
+        for answer in vote.prediction:
+            if (vote.prediction[answer] != 0):
+                predicted_frequencies[answer - 1] += math.log10(vote.prediction[answer])
+                arithmetic_avg_predicted_frequencies[answer - 1] += vote.prediction[answer]
+    for i in range(len(predicted_frequencies)):
+        predicted_frequencies[i] = predicted_frequencies[i] / len(votes)
+        arithmetic_avg_predicted_frequencies[i] = arithmetic_avg_predicted_frequencies[i] / len(votes)
+        geometric_avg_predicted_frequencies[i] = 10**predicted_frequencies[i]
+        print(F"Geometric average of predictions for answer {i + 1}: {geometric_avg_predicted_frequencies[i]}")
+    for i in range(len(arithmetic_avg_predicted_frequencies)):
+        print(F"Arithmetic average of predictions for answer {i + 1}: {arithmetic_avg_predicted_frequencies[i]}")
+    
+    
+    
             
 # Let's say there's a multiple choice question with 4 options
 # The first argument of the Vote object is the answer that the respondent has chosen (1 - 4)
@@ -21,12 +68,14 @@ prediction2 = { 1: 10, 2: 20, 3: 70, 4: 0 }
 prediction3 = { 1: 5, 2: 55, 3: 15, 4: 25 }
 prediction4 = { 1: 0, 2: 100, 3: 0, 4: 0 }
 prediction5 = { 1: 25, 2: 25, 3: 25, 4: 25 }
+prediction6 = { 1: 10, 2: 10, 3: 10, 4 : 70 }
 v1 = Vote(2, prediction1)
 v2 = Vote(3, prediction2)
 v3 = Vote(2, prediction3)
 v4 = Vote(2, prediction4)
 v5 = Vote(2, prediction5)
-votes = [v1, v2, v3, v4, v5]
+v6 = Vote(4, prediction6)
+votes = [v1, v2, v3, v4, v5, v6]
 
-bts(votes)
+bts(4, votes)
 
