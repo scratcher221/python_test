52695ac2c7cbf8b5b356f446654f7f89d603c008
scratcher221 <mitterlehner.david@gmail.com>
2019-08-01 19:24:51 +0200

    Continued the implementation of spa algorithm.

--- cwd_algorithms/spa_test.py
+++ cwd_algorithms/spa_test.py
@@ -2,31 +2,76 @@ from vote import Vote
 import math
 
 def spa(num_answer_options, votes):
-	number_of_votes = len(votes)
+	number_of_respondents = len(votes)
 	# Count votes for each answer option
-	total_votes = {}
-	total_votes_percentage = {}
-	arithmetic_mean_answer = {}
+	total_votes = []
+	total_votes_percentage = []
+	arithmetic_mean_answer = []
+	# For this implementation of the LST principle, we weight respondents equally (w_j = arithmetic_mean_answer[j])
+	weight_answer = []
 	sum_predictions = {}
+	sum_log_predictions = []
 	for i in range(num_answer_options):
-		total_votes[i + 1] = 0
-		arithmetic_mean_answer[i + 1] = 0
+		total_votes.append(0)
+		total_votes_percentage.append(0)
+		arithmetic_mean_answer.append(0)
+		weight_answer.append(0)
 		sum_predictions[i + 1] = 0
+		sum_log_predictions.append([])
+		for j in range(num_answer_options):
+			sum_log_predictions[i].append(0)
 	for vote in votes:
-		total_votes[vote.answer] += 1
+		total_votes[vote.answer - 1] += 1
 	for answer in total_votes:
-		arithmetic_mean_answer[answer] = total_votes[answer] / number_of_votes
-		total_votes_percentage[answer] = (total_votes[answer] / number_of_votes) * 100
-		print(F"Answer {answer} has {total_votes[answer]} votes ({round(total_votes_percentage[answer], 2)}%), x_k = {arithmetic_mean_answer[answer]}.")
+		arithmetic_mean_answer[answer - 1] = total_votes[answer - 1] / number_of_respondents
+		weight_answer[answer - 1] = arithmetic_mean_answer[answer - 1]
+		total_votes_percentage[answer - 1] = (total_votes[answer - 1] / number_of_respondents) * 100
+		print(F"Answer {answer} has {total_votes[answer - 1]} votes ({round(total_votes_percentage[answer - 1], 2)}%), x_k = {arithmetic_mean_answer[answer - 1]}.")
 	print("")
-	respondent_index = 0
+	# Calculate matrix with geometric averages of predictions based on answer of respondent
+	# First create a m x m matrix (m answer options), filled with 0s
+	geo_avg_prediction_matrix = []
+	for i in range(num_answer_options):
+		geo_avg_prediction_matrix.append([])
+		for j in range(num_answer_options):
+			geo_avg_prediction_matrix[i].append(0)
+
+	# Change 0 predictions to 0.1, to prevent domain errors
+	respondent_index = 1
 	for vote in votes:
-		print(f"Prediction from respondent {respondent_index + 1}:")
+		# print(f"Prediction from respondent {respondent_index}:")
 		for answer in vote.prediction:
 			if (vote.prediction[answer] == 0):
 				vote.prediction[answer] = 0.1
-			print(f"Answer: {answer}, Prediction: {vote.prediction[answer]}")
+			# print(f"Answer: {answer}, Prediction: {vote.prediction[answer]}")
 		respondent_index += 1
+
+	# Generate answer matrices to be able to filter predictions by endorsed answer, e.g. answer 3 = [0, 0, 1, 0, 0]
+	answer_matrix = []
+	for i in range(number_of_respondents):
+		answer_matrix.append([])
+		for j in range(num_answer_options):
+			answer_matrix[i].append(0)
+	for i in range(number_of_respondents):
+		answer_matrix[i][votes[i].answer - 1] = 1
+		print(F"Answer matrix for respondent {i + 1}: {answer_matrix[i]}")
+	# Calculate sum of the logarithms of predictions for answer 1 of every respondent who chose answer 1 - 5
+	for j in range(number_of_respondents):
+		for k in range(num_answer_options):
+			sum_log_predictions[0][k] += answer_matrix[j][k] * math.log10(votes[j].prediction[1])
+	for i in range(num_answer_options):
+		print(F"The sum of logarithms of predictions for answer 1 of every respondent who chose answer {i + 1} is: {sum_log_predictions[0][i]}\n")
+
+	for i in range(num_answer_options):
+		for j in range(num_answer_options):
+			if arithmetic_mean_answer[j] != 0:
+				print(F"Calculating geometric average of predictions of answer {i + 1} of respondents who chose answer {j + 1}: 10^(1/({number_of_respondents} * {arithmetic_mean_answer[j]}) * {sum_log_predictions[i][j]})")
+				geo_avg_prediction_matrix[i][j] = 10**(1/(number_of_respondents * arithmetic_mean_answer[j]) * sum_log_predictions[i][j])
+				print(F"The result is: {geo_avg_prediction_matrix[i][j]}")
+			else:
+				geo_avg_prediction_matrix[i][j] = 0
+			# print(F"The geometric average of predictions for answer {i + 1} of every respondent who chose answer {j + 1} is: {geo_avg_prediction_matrix[i][j]}")
+
 		
 	# Calculate the sum of predictions for each answer
 	for vote in votes:
@@ -35,7 +80,7 @@ def spa(num_answer_options, votes):
 	# Divide by the number of votes (each vote contains exactly one prediction), to get average prediction
 	avg_predictions = {}
 	for answer in sum_predictions:
-		avg_predictions[answer] = sum_predictions[answer] / number_of_votes
+		avg_predictions[answer] = sum_predictions[answer] / number_of_respondents
 	# Calculate the geometric average of predictions:
 	geo_avg_predictions = {}
 	for i in range(num_answer_options):
@@ -44,27 +89,12 @@ def spa(num_answer_options, votes):
 		for answer in vote.prediction:
 			geo_avg_predictions[answer] += math.log10(vote.prediction[answer])
 	for i in range(num_answer_options):
-		geo_avg_predictions[i + 1] = 10**(geo_avg_predictions[i + 1] / number_of_votes)
+		geo_avg_predictions[i + 1] = 10**(geo_avg_predictions[i + 1] / number_of_respondents)
 	# Print the average predictions for each answer:
 	print("")
 	print(F"Now printing the average predictions: ")
 	for answer in avg_predictions:
 		print(F"Answer: {answer}, Average Prediction: {round(avg_predictions[answer], 2)}, Geometric Average Prediction: {round(geo_avg_predictions[answer], 2)}")
-		
-	# Calculate the difference between average predictions and actual votes for each answer
-	diff_predictions = {}
-	for answer in total_votes_percentage:
-		diff_predictions[answer] = total_votes_percentage[answer] - avg_predictions[answer]
-		
-	# Print the difference of actual votes (percentage) - average predictions
-	print("")
-	print(F"Now printing the difference of actual votes - average predictions:")
-	for answer in diff_predictions:
-		print(F"Answer: {answer}, Actual Votes: {round(total_votes_percentage[answer], 2)}%, Average Prediction: {round(avg_predictions[answer], 2)}%, Difference: {round(diff_predictions[answer], 2)}")
-		
-	# Get the maximum of the differences
-	max_difference_answer = max(diff_predictions, key=lambda key: diff_predictions[key])
-	print(F"Surprisingly popular answer is: {max_difference_answer}")
 
 # Let's say there's a multiple choice question with 5 options
 # The first argument of the Vote object is the answer that the respondent has chosen (1 - 5)
